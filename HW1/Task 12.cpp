8ab587cdabb47fe28a5cfdc0c3185e852c3d9c16
Владислав Матвиенко <114211276+vladmatvienko@users.noreply.github.com>
2023-12-01 23:56:45 +0300

    Add files via upload
    Добавлены пояснения в 12 задании

--- HW1/Task 12.cpp
+++ HW1/Task 12.cpp
@@ -7,16 +7,16 @@ int main()
 	double Weight = 0; //Вес
 	double Height = 0; //Рост
 	std::cout << "Your weight =  ";
-	std::cin >> Weight;
+	std::cin >> Weight;//Ввод веса
 	std::cout << "Your height =  ";
-	std::cin >> Height;
-	double ideal = Height - 100;
+	std::cin >> Height;//Ввод роста
+	double ideal = Height - 100;// Идеальный вес
+	//Рекомендации в зависимости от веса
 	if (Weight < ideal)
 		std::cout << "You should eat more!";
-	else if (ideal== Weight)
+	else if (ideal == Weight)
 		std::cout << "You Beautiful!";
 	else if (Weight > ideal)
 		std::cout << "You should start diet and go to the gym";
-
 	return 0;
 }
