6456c6e6d57b6dc98b7e4b19f18d8b4da226f1b8
Владислав Матвиенко <114211276+vladmatvienko@users.noreply.github.com>
2023-12-02 03:08:15 +0300

    Add files via upload

---
+++ HW1/Task 13.cpp
@@ -0,0 +1,24 @@
+﻿//13th task//
+//Программа генерирует два случайных числа в диапазоне от 1 до 9. Тестируемый вводит результат произведения этих чисел. Программа сообщает тестируемому ошибся он или нет. 
+//(Рандом по непонятной причине не работает) ＞﹏＜
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+int main()
+{
+	int Rand1 = 1 + std::rand() / ((RAND_MAX + 1u) / 9);//1st random number
+	int Rand2 = 1 + std::rand() / ((RAND_MAX + 1u) / 9);//2nd random number
+	std::cout << "How much " << Rand1 << " * " << Rand2 << std::endl;
+	int Ans = 0;//Ответ
+	std::cout << "Answer: ";
+	std::cin >> Ans;//Ввод ответа
+	//проверка на верность ответа
+	if (Ans == Rand1 * Rand2) {
+		std::cout << "Good Answer";
+	}
+	else {
+		std::cout << "Bad Answer";
+	}
+	return 0;
+}
---
+++ HW1/Task 15.cpp
@@ -0,0 +1,53 @@
+﻿//15th task//
+//Ввести с клавиатуры номер месяца. Выдать сообщение о названии месяца и времени года.
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int Month = 0; //Номер месяца
+	std::cout << "Number of month: ";
+	std::cin >> Month;//Ввод месяца
+        //Проверка на месяц
+	if (Month == 1) {
+		std::cout << "January winter";
+	}
+	else if(Month == 2) {
+		std::cout << "February winter";
+	}
+	else if (Month == 3) {
+		std::cout << "March spring";
+	}
+	else if (Month == 4) {
+		std::cout << "April spring";
+	}
+	else if (Month == 5) {
+		std::cout << "May spring";
+	}
+	else if (Month == 6) {
+		std::cout << "June summer";
+	}
+	else if (Month == 7) {
+		std::cout << "July summer";
+	}
+	else if (Month == 8) {
+		std::cout << "August summer";
+	}
+	else if (Month == 9) {
+		std::cout << "September fall";
+	}
+	else if (Month == 10) {
+		std::cout << "October fall";
+	}
+	else if (Month == 11) {
+		std::cout << "November fall";
+	}
+	else if (Month == 12) {
+		std::cout << "December winter";
+	}
+	else {
+		std::cout << "Not month";
+	}
+
+	return 0;
+}
